8fb85518e57eaac43c125dde9bb6d72b89d6ad5f
帅 胡 <292695505@qq.com>
2020-03-03 13:31:26 +0800

    springcloud注册中心Eureka demo

---
+++ springcloud-eureka/spring-cloud-eureka-cluster/src/main/java/com/hus/springcloud/SpringCloudEurekaApplication.java
@@ -0,0 +1,17 @@
+package com.hus.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * 添加启动代码中添加@EnableEurekaServer注解
+ * @author hus
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringCloudEurekaApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEurekaApplication.class, args);
+	}
+}
